17a9d01ae3abcc12792fc5888d6ca4a588fb4dda
dChakr <54780386+dChakr@users.noreply.github.com>
2019-09-26 21:28:40 +0100

    Updated ArdCode 26/9/19
    code works logically - problem with power on machine, so robot not working

--- ArdCode/ArdCode.ino
+++ ArdCode/ArdCode.ino
@@ -1,4 +1,4 @@
-//Arduino Code v.2 - 14/09/19
+//Arduino Code v.2 - 26/09/19
 
 #include <Servo.h>
 #include <SD.h>
@@ -14,41 +14,32 @@ Servo penServo;
 File SD_File;
 File Size_file;
 
-int servoPin = 30;
+int servoPin = 35;
 unsigned int tPeriod = 600; //change when pointillism process is sorted
 
 int speedSig = 200; //as a result of PID.Compute() from tests
-unsigned int cmTime = 460;
-
+unsigned int cmTime = 500;
+int rowNum = 0;
+int colNum = 0;
+int repos = 0;
 
 void setup() {
-  int repos = 0;
   SD.begin(CS_PIN);
 
   //Variables for reading in size.txt file
   int size[2];
   String element = "";
-  int rowNum = 0;
-  int colNum = 0;
   int a = 0; //indexing variable
 
   //size.txt file handling to find rowNum and colNum
   Size_file = SD.open("size.txt");
-  while(Size_file.available() && i<2){
+  while(Size_file.available() && a<2){
   element = Size_file.readStringUntil('\r');
-  size[i] = element.toInt();
-  i++;
+  size[a] = element.toInt();
+  a++;
   }
   rowNum = size[0];
   colNum = size[1];
-  
-  //Variables for reading picCode.txt file
-  int r = 0;
-  int c = 0;
-  int dotDen[rowNum][colNum];
-  size_t n;
-  char str[20];
-  char *ptr; //constant pointer to non-constant character
   
   //Setting up pins for motors
   pinMode(xPin1, OUTPUT);
@@ -61,41 +52,34 @@ void setup() {
   repos = cmTime*14;
   
   //Serial.begin(9600); //leave for debugging
+}
 
-  //picCode file handling:
+void loop() {
+  int i = 0;
+  int j = 0;
+  int dotVal = 0;
+  int cTime = 0;
+
+//Variables for reading picCode.txt file
+  int r = 0;
+  int c = 0;
+  int dotDen[rowNum][colNum];
+  size_t n;
+  char str[20];
+  char *ptr; //constant pointer to non-constant character
+  
+  //picCode file handling
   SD_File = SD.open("picCode.TXT");
 
   for (r = 0; r < rowNum; r++) {
     for (c = 0; c < colNum; c++) {
       n = readField(&SD_File, str, sizeof(str), ",\n");
-      if (n == 0) {
-        Serial.println("Too few lines");
-      }
-      dotDen[r][c] = strtol(str, &ptr, 10);
-      
-      /*if (ptr == str) { //leave in for debugging - remove at the end
-        Serial.println("bad number");
-      }      
-      if (j < (COL_DIM-1) && str[n-1] != ',') { //leave in for debugging - remove at end
-        Serial.println("line with too few fields");
-      }
-    }
-    // Allow missing endl at eof.
-    if (str[n-1] != '\n' && SD_File.available()) { //leave in for debugging
-      Serial.println("missing endl");
-    */}    
+      dotDen[r][c] = strtol(str, &ptr, 10);}    
   }
-
- SD_File.close();
   
-}
+  SD_File.close();
 
-
-void loop() {
-
-  int i = 0;
-  int j = 0;
-
+ //actual movement of machine
   for (int i = 0; i < rowNum; i++){
     j = 0;
     for (int j = 0; j < colNum; j++){
@@ -103,7 +87,19 @@ void loop() {
       analogWrite(yPin2, 0);//move y-axis in cm increments
       delay(cmTime);
       analogWrite(yPin1, 0);
-      Pointillism();
+
+      dotVal = dotDen[i][j];
+      cTime = Pointillism(dotVal);
+
+      if (dotVal != 1){
+        for (int track = 0; track<(dotVal-1); track++){
+        penServo.write(90);
+        delay(cTime);
+        penServo.write(45);
+        delay(500);
+        }
+      }
+      
       delay(1000);
     }
     analogWrite(yPin2, 240); //reposition y-axis
@@ -117,6 +113,7 @@ void loop() {
     analogWrite(xPin1, 0);
   }
 
+  delay(100000000000);
 }
 
 size_t readField(File* SD_File, char* str, size_t size, char* delim) {
@@ -136,12 +133,39 @@ size_t readField(File* SD_File, char* str, size_t size, char* delim) {
   return n;
 }
 
-void Pointillism() {
-    unsigned long tStart = millis();
-    while((millis()-tStart) < tPeriod){
-      penServo.write(90);
-      delay(300);
-      penServo.write(45);
-      delay(300);
+int Pointillism(int dotVal) {
+  int track = 0;
+  int cTime = 0;
+  
+  switch (dotVal){
+    case 2://nothing happens if dotVal = 1, so neglected
+      cTime = 500;
+      break;
+    case 3:
+      cTime = 1000;
+      break;
+    case 4:
+      cTime = 1500;
+      break;
+    case 5:
+      cTime = 2000;
+      break;
+    case 6:
+      cTime = 2500;
+      break;
+    case 7:
+      cTime = 3000;
+      break;
+    case 8:
+      cTime = 3500;
+      break;
+    case 9:
+      cTime = 4000;
+      break;
+    case 10:
+      cTime = 4500;
+      break;    
   }
+
+  return cTime;
 }
